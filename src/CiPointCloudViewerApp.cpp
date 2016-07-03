a8f0b6fd7969c69e05203e73bfeee972807bbbe2
izumin5210 <masayuki@izumin.info>
2016-07-03 21:54:59 +0900

    Upgrade UI design

--- src/CiPointCloudViewerApp.cpp
+++ src/CiPointCloudViewerApp.cpp
@@ -28,8 +28,19 @@ public:
     void draw() override;
 
 private:
+    const ImGuiWindowFlags kWindowFlags = ImGuiWindowFlags_ShowBorders;
     const int kWindowSpacing = 8;
     const int kWindowWidth = 360;
+
+    const ColorA8u kColorBlackA55   = ColorA8u(0x22, 0x22, 0x22, 0x55);
+    const ColorA8u kColorBlackAcc   = ColorA8u(0x22, 0x22, 0x22, 0xcc);
+    // bleu de provence
+    const ColorA8u kColorPrimary    = ColorA8u(0x00, 0x9a, 0xc5, 0xcc);
+    const ColorA8u kColorPrimaryA99 = ColorA8u(0x00, 0x9a, 0xc5, 0x99);
+    const ColorA8u kColorPrimaryA22 = ColorA8u(0x00, 0x9a, 0xc5, 0x22);
+    // rosso di toscana
+    const ColorA8u kColorAccent     = ColorA8u(0xf1, 0x67, 0x3f, 0xee);
+    const ColorA8u kColorAccentAcc  = ColorA8u(0xf1, 0x67, 0x3f, 0xcc);
 
     map<fs::path, shared_ptr<grabber::PointCloudGrabber>> grabbers_;
 
@@ -51,7 +62,7 @@ private:
     float point_size_   = 1.0f;
     bool visible_grid_  = true;
 
-    Color bg_color_ = Color(0, 0, 0);
+    Color bg_color_ = Color8u(0x11, 0x11, 0x11);
 
     float voxel_size_           = 0.01f;
     bool enabled_voxel_filter_  = false;
@@ -77,7 +88,42 @@ void CiPointCloudViewerApp::setup()
 {
     batch_ = gl::VertBatch::create(GL_POINTS);
 
-    ui::initialize();
+    auto options = ui::Options()
+        .darkTheme()
+        .color(ImGuiCol_MenuBarBg,              kColorPrimaryA22)
+        .color(ImGuiCol_TitleBg,                kColorPrimaryA22)
+        .color(ImGuiCol_TitleBgCollapsed,       kColorPrimaryA22)
+        .color(ImGuiCol_TitleBgActive,          kColorPrimaryA99)
+        .color(ImGuiCol_WindowBg,               kColorPrimaryA22)
+        .color(ImGuiCol_Border,                 kColorPrimaryA99)
+        .color(ImGuiCol_FrameBg,                kColorPrimaryA22)
+        .color(ImGuiCol_FrameBgHovered,         kColorAccentAcc)
+        .color(ImGuiCol_FrameBgActive,          kColorAccent)
+        .color(ImGuiCol_ScrollbarBg,            kColorPrimaryA22)
+        .color(ImGuiCol_ScrollbarGrab,          kColorPrimaryA99)
+        .color(ImGuiCol_ScrollbarGrabHovered,   kColorPrimaryA99)
+        .color(ImGuiCol_ScrollbarGrabActive,    kColorPrimary)
+        .color(ImGuiCol_CheckMark,              kColorAccent)
+        .color(ImGuiCol_SliderGrab,             kColorPrimaryA99)
+        .color(ImGuiCol_SliderGrabActive,       kColorPrimary)
+        .color(ImGuiCol_Button,                 kColorBlackA55)
+        .color(ImGuiCol_ButtonHovered,          kColorAccentAcc)
+        .color(ImGuiCol_ButtonActive,           kColorAccent)
+        .color(ImGuiCol_Header,                 kColorBlackA55)
+        .color(ImGuiCol_HeaderHovered,          kColorAccentAcc)
+        .color(ImGuiCol_HeaderActive,           kColorAccent)
+        .color(ImGuiCol_Column,                 kColorBlackA55)
+        .color(ImGuiCol_ColumnHovered,          kColorAccentAcc)
+        .color(ImGuiCol_ColumnActive,           kColorAccent)
+        .color(ImGuiCol_Text,                   kColorPrimary)
+        .color(ImGuiCol_TextDisabled,           kColorBlackA55)
+        .color(ImGuiCol_TextSelectedBg,         kColorAccent)
+        .color(ImGuiCol_PopupBg,                kColorBlackAcc)
+        .antiAliasedLines(true)
+        .antiAliasedShapes(true)
+        .windowRounding(0.0f)
+        .frameRounding(0.0f);
+    ui::initialize(options);
 
     gl::enableDepthRead();
     gl::enableDepthWrite();
@@ -193,7 +239,7 @@ void CiPointCloudViewerApp::update()
         ui::SetNextWindowSize(vec2(kWindowWidth, 0));
     }
     if (visible_camera_window_) {
-        ui::ScopedWindow window("Camera");
+        ui::ScopedWindow window("Camera", kWindowFlags);
         ui::DragFloat3("Look at", &camera_target_[0]);
         ui::DragFloat3("Eye point", &camera_eye_point_[0]);
 
@@ -202,7 +248,7 @@ void CiPointCloudViewerApp::update()
         ui::SetNextWindowSize(vec2(kWindowWidth, 0));
     }
     if (visible_appearance_window_) {
-        ui::ScopedWindow window("Appearance");
+        ui::ScopedWindow window("Appearance", kWindowFlags);
         ui::InputFloat("Point size", &point_size_, 0.1f);
         ui::ColorEdit3("Background color", &bg_color_[0]);
         windowPos.y += ui::GetWindowHeight() + kWindowSpacing;
@@ -210,7 +256,7 @@ void CiPointCloudViewerApp::update()
         ui::SetNextWindowSize(vec2(kWindowWidth, 0));
     }
     if (visible_filters_window_) {
-        ui::ScopedWindow window("Filters");
+        ui::ScopedWindow window("Filters", kWindowFlags);
         int id = 0;
         auto addFilter = [&](const char *label, std::function<void()> definition) {
             if (ui::TreeNode(label)) {
@@ -290,8 +336,8 @@ void CiPointCloudViewerApp::update()
         ui::SetNextWindowSize(vec2(kWindowWidth, 0));
     }
     if (visible_clouds_window_) {
-        ui::ScopedWindow window("Clouds");
-        ui::ListBoxHeader("");
+        ui::ScopedWindow window("Clouds", kWindowFlags);
+        ui::ListBoxHeader("");          
         for (auto pair: grabbers_) {
             ui::Selectable(pair.first.filename().c_str());
         }
@@ -302,7 +348,7 @@ void CiPointCloudViewerApp::update()
         ui::SetNextWindowSize(vec2(kWindowWidth, 0));
     }
     if (visible_debug_window_) {
-        ui::ScopedWindow window("Debug");
+        ui::ScopedWindow window("Information", kWindowFlags);
         ui::LabelText("FPS", "%f", getAverageFps());
         ui::LabelText("Cloud size", "%d", cloud_size_);
         ui::LabelText("Filtered", "%d", filtered_cloud_size_);
